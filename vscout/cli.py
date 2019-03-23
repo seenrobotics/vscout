451bf03000e8cba7c6308560c47a23e349a0d1b0
seendsouza <sdammobubbles@gmail.com>
2019-03-22 23:19:35 -0400

    fix: fix styling for all current Python files

--- vscout/cli.py
+++ vscout/cli.py
@@ -4,17 +4,20 @@ import sys
 from misc import commands as misc
 from graph import commands as graph
 
+
 def main():
     try:
         ctx_obj = dict()
-        entry_point.main(prog_name='vscout',obj=ctx_obj)
+        entry_point.main(prog_name='vscout', obj=ctx_obj)
     except KeyboardInterrupt:
         click.echo('Aborted!')
 
+
 @click.group()
-@click.version_option(version="0.0.1") 
+@click.version_option(version="0.0.1")
 def entry_point():
     pass
+
 
 entry_point.add_command(graph.graph)
 
--- vscout/graph/commands.py
+++ vscout/graph/commands.py
@@ -1,9 +1,11 @@
 import click
 
-@click.group(short_help = 'Graph team statistics')
+
+@click.group(short_help='Graph team statistics')
 @click.argument('stat')
 def graph(stat):
     click.echo(stat)
+
 
 @graph.command()
 def ls():
