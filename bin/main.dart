dc009875e90d45aa962c251c4435dad228ce7683
seendsouza <sdammobubbles@gmail.com>
2019-07-10 12:28:42 -0400

    refactor: add basic cli command and subcommand
    Rename entrypoint main.dart to vscout_cli.dart
    creates folder structure for all commands
    adds the 'add' command and data_type subcommand
    removes test file
    
    closes issue #2

--- bin/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:vscout_cli/vscout_cli.dart' as vscout_cli;
-
-main(List<String> arguments) {
-  print('Hello world: ${vscout_cli.calculate()}!');
-}
--- test/vscout_cli_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:vscout_cli/vscout_cli.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
