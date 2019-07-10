f52f233509090b24f3b1752497bb74aae9008cbb
seendsouza <sdammobubbles@gmail.com>
2019-07-10 10:28:41 -0400

    refactor: replace python cli with dart base
    closes issue #2

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:vscout_cli/vscout_cli.dart' as vscout_cli;
+
+main(List<String> arguments) {
+  print('Hello world: ${vscout_cli.calculate()}!');
+}
---
+++ test/vscout_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:vscout_cli/vscout_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
