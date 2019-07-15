d9b7c31b79270e51727ed4a009b9681a88748d2f
Jeffrey Li <dr.cheddar@outlook.com>
2019-07-14 21:08:35 -0400

    Merge forgot to add some files

--- lib/src/commands/commands.dart
+++ lib/src/commands/commands.dart
@@ -1,13 +1,10 @@
 library vscout_cli.commands;
 
 export 'add.dart';
-<<<<<<< HEAD
 export 'get.dart';
-=======
 export 'config.dart';
 export 'find.dart';
 export 'init.dart';
 export 'ls.dart';
 export 'rm.dart';
 export 'show.dart';
->>>>>>> 7245f1b3b23926ceb9936d2a6d7c72a33740664f
