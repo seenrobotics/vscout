86db43748e70e249b96eabe28b5b6649c8632c93
Jeffrey Li <dr.cheddar@outlook.com>
2019-08-04 17:43:34 -0400

    feat: create database when command is run if not exists closes issue #25

--- lib/src/database/database.dart
+++ lib/src/database/database.dart
@@ -5,25 +5,39 @@ import 'package:sembast/sembast_io.dart';
 import 'package:path/path.dart';
 import 'package:uuid/uuid.dart';
 
-String dbPath = '/../database/vscout.db';
-var pathToDb = (dirname(Platform.script.toFilePath()).toString() + dbPath);
-
 class DatabaseHandler {
   Database db;
   Store store;
-
+  File databaseFile;
+  String relativeDatabasePath;
+  String realDatabasePath;
   DatabaseHandler() {
-    // Constructors can't call async functions, proper initialization is done in [InitializeDb()].
+    // Constructors can't call async functions, actual initialization is done in [InitializeDb()].
   }
 
   InitializeDb() async {
-    this.db = await this.newDb();
+    //Set the path to the database
+    this.relativeDatabasePath = '/../database/vscout.db';
+    this.realDatabasePath = (dirname(Platform.script.toFilePath()).toString() +
+        this.relativeDatabasePath);
+    this.db = await this.openDb();
     this.SetStore('main');
     return true;
   }
 
-  Future newDb() async {
-    this.db = await databaseFactoryIo.openDatabase(pathToDb);
+  Future createDatabaseFile() async {
+    return (await this.databaseFile.create() is File) ? true : false;
+  }
+
+  Future openDb() async {
+    //Check if database file exists and create it if not.
+    this.databaseFile = new File(this.realDatabasePath);
+    bool databaseExists = await this.databaseFile.exists();
+    databaseExists = databaseExists ? true : await this.createDatabaseFile();
+    //Wait for database file to be created, proceed to open it.
+    this.db = databaseExists
+        ? await databaseFactoryIo.openDatabase(this.realDatabasePath)
+        : false;
     return this.db;
   }
 
