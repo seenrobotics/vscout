1a9c26d6fc102820ebd2a19a31447bc83b7bb4a6
Sean D'Souza <sdammobubbles@gmail.com>
2019-08-20 23:42:39 -0400

    feat: add first tab: vscout

---
+++ lib/src/views/vscout_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:vscout/widgets/vscout_logo_widget.dart';
+
+class VscoutView extends StatefulWidget {
+  @override
+  VscoutViewState createState() {
+    return VscoutViewState();
+  }
+}
+
+class VscoutViewState extends State<VscoutView> {
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () async => true, //false
+        child: Scaffold(
+            appBar: AppBar(
+              title: VscoutLogoWidget(color: Colors.white, size: 24.0),
+              leading: null,
+              automaticallyImplyLeading: false,
+              centerTitle: true,
+            ),
+            backgroundColor: Colors.white,
+            body: SingleChildScrollView(
+                child: SafeArea(
+                    child: Column(children: <Widget>[
+              TextField(),
+            ])))));
+  }
+}
