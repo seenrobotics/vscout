e456cd07383115f6cd5bb43add4ce9a57cbf12c8
Jeffrey Li <dr.cheddar@outlook.com>
2019-08-31 13:56:06 -0400

    style: add comments and small refactors

--- lib/database.dart
+++ lib/database.dart
@@ -1,3 +1,6 @@
 library vscout.database;
 
 export 'src/database/mainDatabaseHandler.dart';
+export 'src/database/filterHandler.dart';
+export 'src/database/databaseHandler.dart';
+export 'src/database/modifyHandler.dart';
